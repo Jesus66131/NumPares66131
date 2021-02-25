ecf1eb1b60dd14b0e27f916d4be177bc39662e2b
Jesus <Jesus@LAPTOP-9O0FI901>
2021-02-24 20:36:55 -0600

    Primer Commit

---
+++ src/numpares66131/NumPares66131.java
@@ -0,0 +1,16 @@
+package numpares66131;
+
+public class NumPares66131 {
+
+    public static void main(String[] args) {
+        
+        int i = 1, j=0;
+        
+        
+        while(i<=47){
+            System.out.println(i+": "+j);
+            j+=2;
+            i++;
+        }
+    }
+}
